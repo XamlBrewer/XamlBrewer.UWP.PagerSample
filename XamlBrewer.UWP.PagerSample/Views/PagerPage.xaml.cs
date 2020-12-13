1a750665da17dc27cc6f02945dfd2c818ea32d07
Diederik Krols <diederik_krols@hotmail.com>
2020-12-13 11:40:11 +0100

    Add project files.

---
+++ XamlBrewer.UWP.PagerSample/Views/PagerPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using WinUI = Microsoft.UI.Xaml.Controls;
+
+namespace XamlBrewer.UWP.PagerSample.Views
+{
+    public sealed partial class PagerPage : Page
+    {
+        public PagerPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public List<string> Pictures => new List<string>
+        {
+            "/Assets/Pictures/picture_1.png",
+            "/Assets/Pictures/picture_2.png",
+            "/Assets/Pictures/picture_3.png",
+            "/Assets/Pictures/picture_4.png",
+            "/Assets/Pictures/picture_5.png",
+            "/Assets/Pictures/picture_6.png",
+            "/Assets/Pictures/picture_7.png",
+            "/Assets/Pictures/picture_8.png"
+        };
+
+        private void Pager_SelectedIndexChanged(
+            WinUI.PagerControl sender, 
+            WinUI.PagerControlSelectedIndexChangedEventArgs args)
+        {
+            ImageRepeater.SelectedIndex = args.NewPageIndex;
+        }
+
+        private void ImageRepeater_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Pager.SelectedPageIndex = (sender as FlipView).SelectedIndex;
+        }
+    }
+}
